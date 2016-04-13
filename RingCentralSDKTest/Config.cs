902742ac4a166f78d12ac804dc89a8ae05c38e8c
Tyler Long <tyler4long@gmail.com>
2016-04-13 11:25:28 +0800

    Initial commit

---
+++ RingCentralSDKTest/Config.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace RingCentralSDKTest
+{
+    class Config
+    {
+        private static Config instance = null;
+        private Config() { }
+
+        public static Config Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    var configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ringcentral-config.json");
+                    using (var sr = new StreamReader(configPath))
+                    {
+                        var jsonData = sr.ReadToEnd();
+                        instance = JsonConvert.DeserializeObject<Config>(jsonData);
+                    }
+                }
+                return instance;
+            }
+        }
+
+        [JsonProperty("appKey")]
+        public string AppKey;
+
+        [JsonProperty("appSecret")]
+        public string AppSecret;
+
+        [JsonProperty("server")]
+        public string Server;
+
+        [JsonProperty("username")]
+        public string Username;
+
+        [JsonProperty("password")]
+        public string Password;
+
+        [JsonProperty("receiver")]
+        public string Receiver;
+
+        public string ServerUrl
+        {
+            get
+            {
+                if (Server == "production")
+                {
+                    return "https://platform.ringcentral.com";
+                }
+                else
+                {
+                    return "https://platform.devtest.ringcentral.com";
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format(@"app key: {0}
+app secret: {1}
+server: {2}
+username: {3}
+password: {4}
+receiver: {5}
+server url: {6}
+", AppKey, AppSecret, Server, Username, Password, Receiver, ServerUrl);
+        }
+    }
+}
---
+++ RingCentralSDKTest/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using RingCentral.SDK;
+using RingCentral.SDK.Http;
+using IO.Swagger.Model;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace RingCentralSDKTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Platform platform = null;
+        private void Authenticate()
+        {
+            if (platform == null)
+            {
+                platform = new SDK(Config.Instance.AppKey, Config.Instance.AppSecret, Config.Instance.ServerUrl, "Test App", "1.0.0").GetPlatform();
+            }
+            if (!platform.IsAuthorized())
+            {
+                platform.Authorize(Config.Instance.Username, null, Config.Instance.Password, true);
+            }
+        }
+
+        private void Send_a_SMS(object sender, RoutedEventArgs e)
+        {
+            Authenticate();
+            var requestModel = new CreateSMSMessage {
+                Text = "Hello World",
+                From = new MessageStoreCallerInfoRequest { PhoneNumber = Config.Instance.Username },
+                To = new List<MessageStoreCallerInfoRequest> {
+                    new MessageStoreCallerInfoRequest { PhoneNumber = Config.Instance.Receiver }
+                },
+            };
+            var request = new Request("/restapi/v1.0/account/~/extension/~/sms", JsonConvert.SerializeObject(requestModel));
+            var response = platform.Post(request);
+            var responseJson = response.GetBody();
+            var responseModel = JsonConvert.DeserializeObject<GetMessageInfoResponse>(responseJson);
+        }
+
+        private void Query_Call_Log(object sender, RoutedEventArgs e)
+        {
+            Authenticate();
+        }
+
+        private void Query_Message_Store(object sender, RoutedEventArgs e)
+        {
+            Authenticate();
+        }
+
+        private void Update_Message_Read_Status(object sender, RoutedEventArgs e)
+        {
+            Authenticate();
+        }
+    }
+}
